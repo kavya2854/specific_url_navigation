0f9f88fd8a7c51291a72236dfbf7a3d00306d507
kavya2854 <gotlurukavya@gmail.com>
2023-11-21 19:53:57 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def navya(request):
+    return render(request,'navya.html')
+
+def kavya(request):
+    return render(request,'kavya.html')
